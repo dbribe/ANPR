0b64f05224b5274b6d306fed970927676844576f
Denis <kir141996@gmail.com>
2018-06-22 13:41:43 +0300

    * Sped up at 2s per image or so.

--- code/compute_roi.py
+++ code/compute_roi.py
@@ -1,26 +1,38 @@
 import numpy as np
 import os
+from skimage import measure
 import queue
 from recognize import single_decode
 from PIL import Image, ImageEnhance
 import matplotlib.pyplot as plt
+from generate_input import regions
 from matplotlib.patches import Rectangle
 from scipy.ndimage.filters import gaussian_filter
 from config import MERGE_STRIDE, REGION_THRESHOLD, LIGHT_THRESHOLD, REGION_STRIDE, FILL_THRESHOLD, IGNORE_THRESHOLD, \
     ROI_LIGHT_THRESHOLD, ROI_AVG_THRESHOLD
 
-cnt = 0
+total = 0
 tot_rects = 0
 PLOT = True
+
+class Blob(object):
+    xa = 1e9
+    ya = 1e9
+    xb = 0
+    yb = 0
+    xc1 = 0
+    xc2 = 0
+    yc1 = -1e9
+    yc2 = -1e9
 
 def main():
 
 
     def compute_roi(A, img):
-        global cnt
+        global total
         global PLOT
         global tot_rects
-        cnt += 1
+        total += 1
         # A = gaussian_filter(A, 2)
         visited = np.zeros(np.shape(A))
         B = A
@@ -28,14 +40,14 @@ def main():
         width = np.size(A,1)
         max_queue_size = height * width
 
+        fig, ax = plt.subplots(2)
         if PLOT:
-            fig, ax = plt.subplots(3)
+
             plt.gray()
-        # ax = [ax]
+            # ax = [ax]
         rect_count = 0
         # ax.imshow(A)
 
-        # ax[1].imshow(B)
         mr = MERGE_STRIDE
 
         # print(light_threshold, dark_threshold)
@@ -47,127 +59,130 @@ def main():
                 if A[i][j] > light_threshold:
                     B[i][j] = min(np.amax(A[i-mr+1:i+mr, j-mr+1:j+mr]) * 1.3, 1)
 
-        if PLOT:
-            ax[0].imshow(img)
-            ax[2].imshow(B)
-        st = REGION_STRIDE
+        ax[0].imshow(img)
 
-        for ii in range((int)(height / st)):
-            for jj in range((int)(width / st)):
-                i = ii * st
-                j = jj * st
-                if not visited[i][j] and B[i][j] > FILL_THRESHOLD:
-                    ya = i
-                    xa = j
-                    yb = i
-                    xb = j
-                    mi = B[i][j]
-                    ma = B[i][j]
-                    qx = queue.Queue(max_queue_size)
-                    qy = queue.Queue(max_queue_size)
-                    qx.put(j)
-                    qy.put(i)
-                    visited[i][j] = 1
-                    count = 1
-                    ignored_count = 0
-                    total = 0
-                    xc1 = j
-                    yc1 = i
-                    xc2 = j
-                    yc2 = i
-                    while not qx.empty() and not qy.empty():
-                        # print('eliminated')
-                        xc = qx.get()
-                        yc = qy.get()
-                        for [dx, dy] in [[0, st], [st, 0], [0, -st], [-st, 0]]:
-                            # print(dx)
-                            # print(dy)
-                            xn = xc + dx
-                            yn = yc + dy
-                            # print(yn,xn)
-                            if xn < 0 or xn >= width or yn < 0 or yn >= height or \
-                                    B[yn][xn] - mi > REGION_THRESHOLD or ma - B[yn][xn] > REGION_THRESHOLD or \
-                                    visited[yn][xn] == 1:
-                                continue
-                            total += B[yn][xn]
-                            visited[yn][xn] = 1
-                            qx.put(xn)
-                            qy.put(yn)
-                            count += 1
-                            # print('added')
-                            xa = min(xa, xn)
-                            ya = min(ya, yn)
-                            xb = max(xb, xn)
-                            yb = max(yb, yn)
-                            mi = min(mi, B[yn][xn])
-                            ma = max(ma, B[yn][xn])
-                            if yn - xn > yc1 - xc1:
-                                yc1 = yn
-                                xc1 = xn
-                            if yn + xn > yc2 + xc2:
-                                yc2 = yn
-                                xc2 = xn
-                    # print('adding rect')
-                    # print(xb-xa, yb-ya)
-                    rect_width = xb - xa
-                    rect_height = yb - ya
-                    # rect = Rectangle((xa, ya), xb - xa, yb - ya, edgecolor='b', fill=False)
-                    # ax[0].add_patch(rect)
-                    # rect_c
+        blobs = B > FILL_THRESHOLD
+        blobs_labels = measure.label(blobs, background=0, connectivity=1)
+        # if PLOT:
+        #     ax[3].imshow(blobs_labels)
+        # print(blobs_labels)
 
-                    if total / (count - ignored_count) >= ROI_AVG_THRESHOLD and ma > ROI_LIGHT_THRESHOLD \
-                            and rect_width >= 40 and rect_height >= 5 and rect_width >= 2.5 * rect_height \
-                            and rect_width <= 7 * rect_height and \
-                            count > rect_width * rect_height / (st * st * 4) and rect_width >= width / 32:
-                        nxa = int(xa / width * img.size[0])
-                        nxb = int(xb / width * img.size[0])
-                        nya = int(ya / height * img.size[1])
-                        nyb = int(yb/ height * img.size[1])
-                        # print(nxa, nya, nxb, nyb, img.size[0], img.size[1])
-                        cropped = img.crop((nxa, nya, nxb, nyb))
-                        cropped = cropped.rotate(np.arctan((yc2 - yc1) / (xc2 - xc1)) / np.pi * 180, expand=0)
-                        smth = int(abs(yc2 - yc1) / (yb - ya) * (nyb - nya) * .3)
-                        print("smth is", smth)
-                        print(nxa, nya, nxb, nyb)
-                        cropped = cropped.crop((0, smth, (nxb - nxa), (nyb - nya) - smth))
-                        cropped = cropped.resize((128, 64), Image.LANCZOS)
-                        cropped = gaussian_filter(cropped, 0.8)
-                        cropped = np.asarray(cropped)
-                        cR = cropped[:, :, 0]
-                        cG = cropped[:, :, 1]
-                        cB = cropped[:, :, 2]
-                        cropped = np.maximum(np.maximum(cR, cG), cB)
-                        cropped = cropped[:, :] / 255
-                        imsh = cropped
-                        cropped = np.flip(np.rot90(cropped, -1), 1)
-                        cropped = np.expand_dims(cropped, axis=2)
-                        val = single_decode(cropped)
-                        print(val)
-                        if len(val) < 1:
+        B = blobs_labels
+        cnt = np.max(B)
+        list = np.empty(cnt+1, dtype=Blob)
+        for i in range(cnt+1):
+            list[i] = Blob()
+
+        for i in range(np.size(B,0)):
+            for j in range(np.size(B,1)):
+                index = B[i][j]
+                list[index].xa = min(list[index].xa, j)
+                list[index].ya = min(list[index].ya, i)
+                list[index].xb = max(list[index].xb, j)
+                list[index].yb = max(list[index].yb, i)
+                if i - j > list[index].yc1 - list[index].xc1:
+                    list[index].yc1 = i
+                    list[index].xc1 = j
+                if i + j > list[index].yc2 + list[index].xc2:
+                    list[index].yc2 = i
+                    list[index].xc2 = j
+
+        for i in range(1, cnt):
+            xa = list[i].xa
+            ya = list[i].ya
+            xb = list[i].xb
+            yb = list[i].yb
+            xc1 = list[i].xc1
+            xc2 = list[i].xc2
+            yc1 = list[i].yc1
+            yc2 = list[i].yc2
+            rect_width = xb - xa
+            rect_height = yb - ya
+            if  rect_width >= 30 and rect_height >= 3 and rect_width >= 2.5 * rect_height \
+                    and rect_width <= 7 * rect_height and  rect_width >= width / 32:
+                nxa = int(xa / width * img.size[0])
+                nxb = int(xb / width * img.size[0])
+                nya = int(ya / height * img.size[1])
+                nyb = int(yb/ height * img.size[1])
+                # print(nxa, nya, nxb, nyb, img.size[0], img.size[1])
+                cropped = img.crop((nxa, nya, nxb, nyb))
+                cropped = cropped.rotate(np.arctan((yc2 - yc1) / (xc2 - xc1)) / np.pi * 180, expand=0)
+                smth = int(abs(yc2 - yc1) / (yb - ya) * (nyb - nya) * .3)
+                # print("smth is", smth)
+                # print(nxa, nya, nxb, nyb)
+                cropped = cropped.crop((0, smth, (nxb - nxa), (nyb - nya) - smth))
+                cropped = cropped.resize((128, 64), Image.LANCZOS)
+                cropped = gaussian_filter(cropped, 0.8)
+                cropped = np.asarray(cropped)
+                cR = cropped[:, :, 0]
+                cG = cropped[:, :, 1]
+                cB = cropped[:, :, 2]
+                cropped = np.maximum(np.maximum(cR, cG), cB)
+                cropped = cropped[:, :] / 255
+                imsh = cropped
+                cropped = np.flip(np.rot90(cropped, -1), 1)
+                cropped = np.expand_dims(cropped, axis=2)
+                val = single_decode(cropped)
+                # print(val)
+                val = ''.join(val.split('_'))
+
+                if len(val) > 0 and str.isdigit(val[0]):
+                    val = 'B' + val
+                if len(val) < 6:
+                    continue
+                if not str.isalpha(val[-1]) or not str.isalpha(val[-2]) or not str.isalpha(val[-3]):
+                    continue
+                while len(val) > 3 and str.isalpha(val[-4]):
+                    val = val[0:len(val) - 1]
+                if val[0] == 'F':
+                    val = 'I' + val
+                if (val[0] == 'D' or val[0] == 'E') and str.isdigit(val[1]):
+                    val = 'B' + val[1:len(val)]
+                if str.isalpha(val[0]) and str.isalpha(val[1]) and (val[0] == 'B' or val[1] == 'B' and not (val[0:2] in regions)):
+                    val = 'B' + val[2:len(val)]
+                if len(val) < 6 or len(val) > 7:
+                    continue
+                if val[0:2] in regions and len(val) == 7:
+                    if str.isdigit(val[2]) and str.isdigit(val[3]) and str.isalpha(val[4]) and str.isalpha(val[5]) and str.isalpha(val[6]):
+                        val = val
+                    else:
+                        continue
+                elif val[0:1] == 'B':
+                    if len(val) == 6:
+                        if str.isdigit(val[1]) and str.isdigit(val[2]) and str.isalpha(val[3]) and str.isalpha(val[4]) and str.isalpha(val[5]):
+                            val = val
+                        else:
                             continue
-                        begins_alpha = str.isalpha(val[0])
-                        ends_alpha = str.isalpha(val[-1])
-                        has_digit = 0
-                        for a in val:
-                            if str.isdigit(a):
-                                has_digit += 1
-                        # cropped.save('/Users/Dragonite/Programming/Repos/ANPR/crops/crp' + str(cnt) + '.png')
-                        if begins_alpha and ends_alpha and has_digit >= 2 and has_digit <= 3 and len(val) >= 6 and len(val) <= 7:
-                            if PLOT:
-                                rect = Rectangle((nxa, nya), nxb - nxa, nyb - nya, edgecolor='springgreen', fill=False)
-                                ax[0].add_patch(rect)
-                                ax[0].text(nxa, nya, val,
-                                        verticalalignment='bottom', horizontalalignment='left',
-                                        # transform=ax[0].transAxes,
-                                        color='springgreen', fontsize=9)
-                                ax[1].imshow(imsh)
-                            rect_count += 1
-        # print(rect_count)
-        tot_rects += rect_count
-        print(tot_rects, cnt)
+                    elif len(val) == 7 and str.isdigit(val[1]) and str.isdigit(val[2]) and str.isdigit(val[3]) and str.isalpha(val[4]) and str.isalpha(val[5]) and str.isalpha(val[6]):
+                        val = val
+                    else:
+                        continue
+                else:
+                    continue
+
+
+                # cropped.save('/Users/Dragonite/Programming/Repos/ANPR/crops/crp' + str(cnt) + '.png')
+                if PLOT:
+                    rect = Rectangle((nxa, nya), nxb - nxa, nyb - nya, edgecolor='springgreen', fill=False)
+                    ax[0].add_patch(rect)
+                    ax[0].text(nxa, nya, val,
+                            verticalalignment='bottom', horizontalalignment='left',
+                            bbox=dict(facecolor='black', alpha=0.5),
+                            # transform=ax[0].transAxes,
+                            color='springgreen', fontsize=9)
+                    ax[1].imshow(imsh)
+                rect_count += 1
+
+        if rect_count:
+            fig.savefig('/Users/Dragonite/Programming/Repos/ANPR/detected/' + str(total) + '.png')
+            tot_rects += 1
+        else:
+            img.save('/Users/Dragonite/Programming/Repos/ANPR/undetected/' + str(total) + '.png')
+        print(str(tot_rects / total * 100) + '%', tot_rects, total)
+        plt.close()
         if PLOT:
             plt.show()
-            plt.close()
+
 
     def show_compute_roi(image_path):
         img = Image.open(image_path)
@@ -177,9 +192,13 @@ def main():
         contrast = ImageEnhance.Contrast(img)
         mi = np.asarray(img).min()
         ma = np.asarray(img).max()
-        contrast_ratio = (285 / (ma - mi + 1))
+        contrast_ratio = (305 / (ma - mi + 1))
+        contrast_ratio2 = (275 / (ma - mi + 1))
 
-        orig_img = img = contrast.enhance(contrast_ratio)
+        orig_img = img
+        img = contrast.enhance(contrast_ratio)
+        contrast2 = ImageEnhance.Contrast(orig_img)
+        orig_img = contrast2.enhance(contrast_ratio2)
 
         if img.size[0] > basewidth:
             img = img.resize((basewidth, hsize), Image.LANCZOS)
