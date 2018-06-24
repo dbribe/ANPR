be51019069b5b942635e0184310f649fbaf1b2ca
Denis Mita <11160823+dbribe@users.noreply.github.com>
2018-06-24 19:47:19 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3-pip
+apt-get install libpango1.0-0
+apt-get install libcairo2
+apt-get install libpq-dev
+sudo apt-get install python3-tk
+sudo apt-get install python3-opencv
+python3 -mpip install matplotlib
